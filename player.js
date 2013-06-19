ceac84110c6be25a1ec9c8e1f0eeda1e7f450c44
Mark Ibbotson <uittilla@gmail.com>
2013-06-19 14:16:47 +0200

    Update player.js

--- player.js
+++ player.js
@@ -1,1 +1,31 @@
-var scraper  = scrape.init(country.toLowerCase(), key.toString('utf-8'), language, geoLoc);
+/**
+ * Author mibbotson (ibbo was ere)
+ * ibbo at mail dot com
+ */
+
+var Player = function () {};
+
+Player.prototype = {
+    init: function(id, name) {
+        this.x = (Math.random() * 3000)+1; 
+        this.y = 1000; 
+        this.vr = 0; 
+        this.vx = 0; 
+        this.vy = 0;
+        this._thrust = 0;
+        this.rotation = 0;
+        this.thruster = false;
+        this.shots = [];
+        this.particles = [];
+        this.id = id;
+        this.sheild = false;
+        this.score = 0;
+        this.round = 1;
+        this.name = name;
+        this.lives = 3;
+        this.start_move = 0;
+        return this;
+    }
+};
+
+module.exports = new Player();
